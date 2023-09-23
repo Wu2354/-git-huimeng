8491c71140b1ceadbed133e1c8052ffd7224e214
Wu2354 <136400831+Wu2354@users.noreply.github.com>
2023-09-23 15:39:05 +0800

    1

---
+++ Assets/scirpts/ChangePersonView.cs
@@ -0,0 +1,44 @@
+using StarterAssets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangePersonView : MonoBehaviour
+{
+    [SerializeField] GameObject firstPersonViewGO;
+
+    [SerializeField] GameObject thirdPersonViewGO;
+    private bool IsFirstView = true;
+
+    
+    private void Start()
+    {
+       
+    }
+
+    private void Update()
+    {
+        if (firstPersonViewGO != null && thirdPersonViewGO != null)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Onclick();
+            }
+        }
+        
+    }
+    public void Onclick()
+    {       
+        IsFirstView = !IsFirstView;
+        if (IsFirstView)
+        {           
+            firstPersonViewGO.SetActive(true);
+            thirdPersonViewGO.SetActive(false);
+        }
+        else
+        {
+            firstPersonViewGO.SetActive(false);
+            thirdPersonViewGO.SetActive(true);            
+        }
+
+    }
+}
