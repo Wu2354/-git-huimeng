f8f231e1ca6c61a7c43370ae5c867676dad6c925
Wu2354 <136400831+Wu2354@users.noreply.github.com>
2023-09-26 17:59:14 +0800

    1

--- Assets/scirpts/ChangePersonView.cs
+++ Assets/scirpts/ChangePersonView.cs
@@ -1,33 +1,30 @@
-using StarterAssets;
+
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ChangePersonView : MonoBehaviour
 {
+    [SerializeField] LayerMask boardLayer;//展板在的层
+    [SerializeField] float viewRaduis;//检测半径
     [SerializeField] GameObject firstPersonViewGO;
-
-    [SerializeField] GameObject thirdPersonViewGO;
-    private bool IsFirstView = true;    
-    private void Start()
-    {
-       
-    }
+    [SerializeField] GameObject thirdPersonViewGO;    
+    private bool isViewingBoard = false; //追踪是否正在查看展板    
 
     private void Update()
     {
-        if (firstPersonViewGO != null && thirdPersonViewGO != null)
-        {
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                Onclick();
-            }
-        }
-        
+        CheckForBoardView();
     }
-    public void Onclick()
+    private void CheckForBoardView()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position + new Vector3(0, 1.8f), viewRaduis, boardLayer);
+        foreach (Collider collider in colliders)
+        {
+            Vector3 toHitCollider = collider.transform.position - transform.position;
+        }
+    }
+    public void PersonViewChange()
     {       
-        IsFirstView = !IsFirstView;
-        if (IsFirstView)
+        //IsFirstView = !IsFirstView;
+        if (!isViewingBoard)
         {           
             firstPersonViewGO.SetActive(true);
             thirdPersonViewGO.SetActive(false);
