cc566221b9c6906fe1ace8b091355fd56d04f5ef
Wu2354 <136400831+Wu2354@users.noreply.github.com>
2023-09-27 11:27:36 +0800

    1

--- Assets/scirpts/ChangePersonView.cs
+++ Assets/scirpts/ChangePersonView.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class ChangePersonView : MonoBehaviour
 {
     [SerializeField] LayerMask boardLayer;//展板在的层
-    [SerializeField] float viewRaduis;//检测半径
+    [SerializeField] float viewRaduis = 3f;//检测半径
+    [SerializeField] float angle = 45f;//检测角度
     [SerializeField] GameObject firstPersonViewGO;
-    [SerializeField] GameObject thirdPersonViewGO;    
-    private bool isViewingBoard = false; //追踪是否正在查看展板    
+    [SerializeField] GameObject thirdPersonViewGO;          
 
     private void Update()
     {
@@ -15,16 +15,26 @@ public class ChangePersonView : MonoBehaviour
     }
     private void CheckForBoardView()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position + new Vector3(0, 1.8f), viewRaduis, boardLayer);
-        foreach (Collider collider in colliders)
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position + new Vector3(0, 1.8f), viewRaduis, boardLayer);
+        foreach (Collider collider in hitColliders)
         {
             Vector3 toHitCollider = collider.transform.position - transform.position;
+            float angleToCollider = Vector3.Angle(transform.forward, toHitCollider);
+
+            if(angleToCollider <= angle)
+            {
+                PersonViewChange(true);
+                return; // 找到一个展板在扇形区域内，退出方法
+            }            
         }
+
+        // 如果循环完成并没有返回，说明没有展板在扇形区域内
+        PersonViewChange(false);
     }
-    public void PersonViewChange()
+    public void PersonViewChange(bool isViewingBoard)
     {       
         //IsFirstView = !IsFirstView;
-        if (!isViewingBoard)
+        if (isViewingBoard)
         {           
             firstPersonViewGO.SetActive(true);
             thirdPersonViewGO.SetActive(false);
