ec7ce35e2da519c38ccf1c18e60d6763af591e63
Wu2354 <136400831+Wu2354@users.noreply.github.com>
2023-10-26 17:59:44 +0800

    1

---
+++ Assets/scirpts/DialogSystem/DialogManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    /// <summary>
+    /// 对话文本文件
+    /// </summary>
+    [SerializeField] TextAsset dialogDataFile;
+    /// <summary>
+    /// 左右侧角色图像
+    /// </summary>
+    [SerializeField] Image imageLeft;
+    [SerializeField] Image imageRight;
+    /// <summary>
+    /// 角色名称文本
+    /// </summary>
+    [SerializeField] TMP_Text nameText;
+    /// <summary>
+    /// 对话内容文本
+    /// </summary>
+    [SerializeField] TMP_Text dialogText;
+    /// <summary>
+    /// 角色图片列表
+    /// </summary>
+    [SerializeField] List<Sprite> images = new List<Sprite>();
+    /// <summary>
+    /// 角色名字对应图片的字典
+    /// </summary>
+    Dictionary<string, Sprite> imageDic = new Dictionary<string, Sprite>();
+
+    private void Awake()
+    {
+        imageDic["凌光"] = images[0];
+        imageDic["甘雨"] = images[1];
+    }
+    void Start()
+    {
+        UpdateText("凌光", "欢迎来到学堂");
+        UpdateImage("凌光", true);
+    }
+        
+    void Update()
+    {
+        
+    }
+
+    private void UpdateText(string _name, string _text)
+    {
+        nameText.text = _name;
+        dialogText.text = _text;
+    }
+
+    private void UpdateImage(string _name, bool _atLeft)
+    {
+        if(_atLeft)
+        {
+            imageLeft.sprite = imageDic[_name];
+        }
+        else
+        {
+            imageRight.sprite = imageDic[_name];
+        }
+    }
+}
